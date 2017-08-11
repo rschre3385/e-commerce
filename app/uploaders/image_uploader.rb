f950c27ccb46ef1cd08e9c0b876d8b7ecd07369e
Ryan Schreder <schredermail@yahoo.com>
2017-08-11 14:20:13 -0500

    edited the sws gem and configs

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  
   if Rails.env.production? 
     storage :fog 
   else 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider              => 'AWS',
-    :aws_access_key_id     => "#{ENV['AWS_ACCESS_KEY_ID']}",
-    :aws_secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}",
-    :region                => "#{ENV['S3_REGION']}",
+    :aws_access_key_id     => "#{ENV['AKIAIPXM5YKISEB5VVDA']}",
+    :aws_secret_access_key => "#{ENV['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']}",
+    :region                => "#{ENV['us-east-1']}",
     :path_style            => true
   }
 
-  config.fog_directory =  "#{ENV['S3_BUCKET_NAME']}"
+  config.fog_directory =  "#{ENV['e-commerce-photos-production']}"
   config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
 end
